f7d2e0d756f7c482416047e30783021b5765c819
Jdda777 <juandavidmendozagaspar@gmail.com>
2024-10-01 10:37:57 -0500

    se hizo la card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Cards from "./assets/componentes/Cards.jsx";
+import Card from "./assets/componentes/card/Card.jsx";
+import Video from "./assets/componentes/card/video.jsx";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
+      <Card 
+      lang={"Tiburon"}
+      img={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/TDpGUipa.jpg/1280px-TDpGUipa.jpg"}
+      fcolor={"green"}
+      scolor={"blue"}
+      />
+      <Video />
+      <h1>Juan Mendoza</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
